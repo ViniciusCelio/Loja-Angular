a9675a64951f80a98fbd619cca0339d7a28cce94
ViniciusCelio <viniciusceliofontes@gmail.com>
2025-01-13 19:20:57 -0300

    Adding edit, creat, delete and add buttons to the interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,13 @@ import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
 import { PaginatorModule } from 'primeng/paginator';
+import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule, PaginatorModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule, EditPopupComponent, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -23,6 +25,43 @@ export class HomeComponent {
   totalRecords : number = 0;
 
   rows : number = 5;
+
+  displayEditPopup: boolean = false;
+  displayAddPopup: boolean = false;
+
+  toggleEditPopup(product: Product) {
+    this.selectedProduct = product;
+    this.displayEditPopup = true;
+  }
+
+  toggleDeletePopup(product : Product) {
+    if (!product.id) {
+      return;
+    }
+    this.deleteProduct(product.id);
+  }
+
+  toggleAddPopup() {
+    this.displayAddPopup = true;
+  }
+
+  selectedProduct: Product= {
+    id: 0,
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  }
+
+  onConfirmEdit(product: Product) {
+    this.editProduct(product, this.selectedProduct.id ?? 0);
+    this.displayEditPopup = false;
+  }
+
+  onConfirmAdd(product: Product) {
+    this.addProduct(product);
+    this.displayEditPopup = false;
+  }
 
   onPageChange(event : any) {
     this.fetchProducts(event.page, event.rows);
